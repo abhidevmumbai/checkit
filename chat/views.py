463ee384bf925ef6352472128d78a3aada30c752
Abhishek <abhidevmumbai@gmail.com>
2013-10-24 17:06:28 +0530

    Added a limit for the chat history in the DB and added a link

--- chat/views.py
+++ chat/views.py
@@ -6,11 +6,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.sessions.models import Session
 from django.contrib.auth.decorators import login_required
 
-import redis
+# import redis
 
+chat_limit = 50
 @login_required
 def chat(request):
-    chats = Chat.objects.select_related().all()[0:100]
+    chats = Chat.objects.select_related().all()[0:chat_limit]
     return render(request, 'chat/chat.html', locals())
 
 @csrf_exempt
@@ -20,10 +21,19 @@ def chat_api(request):
         session = Session.objects.get(session_key=request.POST.get('sessionid'))
         user_id = session.get_decoded().get('_auth_user_id')
         user = User.objects.get(id=user_id)
-        
+
         #Create chat message
         Chat.objects.create(user=user, message=request.POST.get('message'))
         
+        # Remove the extra chat messages from the table
+        chat = Chat.objects.all()
+        chat_count = chat.count()
+        count = chat_count - chat_limit
+        
+
+        while count > 0:
+            chat[0].delete()
+            count -= 1
         #Once comment has been created post it to the chat channel
         # r = redis.StrictRedis(host='localhost', port=6379, db=0)
         # message = json.dumps({"user": user.username, "message": request.POST.get('comment')})
@@ -32,4 +42,5 @@ def chat_api(request):
         
         return HttpResponse("Everything worked :)")
     except Exception, e:
+        print e
         return HttpResponseServerError(str(e))
