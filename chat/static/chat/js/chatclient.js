dd3f63e5f5f28d199f06d3e0a2a8238b9704d43b
abhi <abhidevmumbai@gmail.com>
2014-02-11 16:00:31 +0530

    converted the code in raw js

--- chat/static/chat/js/chatclient.js
+++ chat/static/chat/js/chatclient.js
@@ -1,42 +1,67 @@
-$('document').ready(function(){
-	var msgInput = $('#msgInput');
 
-	var socket = io.connect('http://localhost:4000'),
+
+$('document').ready(function(){
+	var msgInput = $('#msgInput'),
 		conversation = $('#conversation');
 
-	// on connection to server, ask for user's name with an anonymous callback
-	socket.on('connect', function(){
-		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		socket.emit('adduser', username);
-	});
+	var ws = new WebSocket("ws://127.0.0.1:8080/");
 
-	// listener, whenever the server emits 'updatechat', this updates the chat body
-	socket.on('updatechat', function (data) {
+	// on connection to server, ask for user's name with an anonymous callback
+	ws.onopen = function(event) {
+	  // The connection was opened
+	  // call the server-side function 'adduser' and send one parameter (value of prompt)
+	  var msg = {
+				  	from: "client",
+				  	type: "adduser",
+				  	message: username
+				};
+	  ws.send(JSON.stringify(msg));
+	}; 
+	ws.onclose = function(event) { 
+	  // The connection was closed
+	  conversation.append('<div class="server_msg msg_box box-green"><b>'+event.data + '</b></div>');
+	}; 
+	ws.onmessage = function(event) {
+		// New message arrived
+		var data = event.data
 		if(data){
-			if(data.type == 'server'){
-				conversation.append('<div class="server_msg msg_box box-green"><b>'+data.message + '</b></div>');	
-			}else if(data.type == 'client'){
-				conversation.append('<div class="client_msg msg_box box-blue"><b>'+data.username + ':</b><div>' + msgParser.init(data.message) + '</div></div>');	
+			switch(data.type) {
+				// listener, whenever the server emits 'updatechat', this updates the chat body
+				case "updatechat":
+					if(data.from == 'server'){
+						conversation.append('<div class="server_msg msg_box box-green"><b>'+data.message + '</b></div>');	
+					}else if(data.from == 'client'){
+						conversation.append('<div class="client_msg msg_box box-blue"><b>'+data.username + ':</b><div>' + msgParser.init(data.message) + '</div></div>');	
+					}
+					break;
+
+				// listener, whenever the server emits 'updateusers', this updates the username list
+				case "updateusers":
+					$('#users').empty();
+					$.each(data, function(key, value) {
+						$('#users').append('<div>' + key + '</div>');
+					});
+					break;
 			}
+
 			conversation.scrollTop(conversation[0].scrollHeight);
 		}
 		return;
-	});
-
-	// listener, whenever the server emits 'updateusers', this updates the username list
-	socket.on('updateusers', function(data) {
-		$('#users').empty();
-		$.each(data, function(key, value) {
-			$('#users').append('<div>' + key + '</div>');
-		});
-	});
-
+	}; 
+	ws.onerror = function(event) { 
+	  // There was an error with your WebSocket
+	  conversation.append('<div class="server_msg msg_box box-green"><b>'+event.data + '</b></div>');
+	};
+	
 	// when the client clicks SEND
 	$('#chatsend').click( function() {
 		var message = msgInput.val();
 		if(message != ''){
 			// tell server to execute 'sendchat' and send along one parameter
-			socket.emit('sendchat', message);
+			ws.send({
+				type: "sendchat",
+				message: message
+			});
 			msgInput.val('');
 		}
 	});
