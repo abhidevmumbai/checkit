dcefaed266c412eb4e8089573593323c307c71cd
Abhishek <abhishek@sportkix.com>
2015-03-22 12:23:23 +0530

    mysql local user creds changed

--- settings.py
+++ settings.py
@@ -44,7 +44,7 @@ else:
             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'checkit',                      # Or path to database file if using sqlite3.
             'USER': 'root',                      # Not used with sqlite3.
-            'PASSWORD': 'root',                  # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
